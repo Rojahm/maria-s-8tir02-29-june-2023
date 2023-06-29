3f4b63e3f98c8f215e2348430e4317a55751b03b
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-06-29 21:14:03 +0330

    contact

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="Contact">
+      <div className="row">
+        <div className="col">
+          <h4>تماس</h4>
+        </div>
+        <div className="col">از تخفیف‌ها و جدیدترین‌ها باخبر شوید:</div>
+        <div className="col"> در شبکه های اجتماعی</div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
