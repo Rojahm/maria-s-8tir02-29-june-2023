a2abf7f730c2b52a9f26d90da39ac0c298643377
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-06-29 17:17:19 +0330

    subtracted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <header className="App-header"></header>
+      <Footer />
     </div>
   );
 }
