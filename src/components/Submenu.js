bdb0dfe3c88fca88921c6e98b5b1da5f0e7c7c14
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-06-29 17:17:52 +0330

    submenu

---
+++ src/components/Submenu.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Submenu = () => {
+  return (
+    <div className="Submenu">
+      <ul className="dropdown-menu">
+        <li>
+          <button className="dropdown-item">Action</button>
+        </li>
+        <li>
+          <button className="dropdown-item">Another action</button>
+        </li>
+        <li>
+          <button className="dropdown-item">Something else here</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Submenu;
