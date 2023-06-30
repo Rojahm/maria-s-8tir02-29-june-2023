c2a4764be85f714df400a80c3648bcc6bb30d6f7
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-06-30 12:40:08 +0330

    farsi

--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "./Submenu.css";
 
 const Submenu = () => {
   return (
     <div className="Submenu">
       <ul className="dropdown-menu">
         <li>
-          <button className="dropdown-item">Action</button>
+          <button className="dropdown-item">دسته بندی</button>
         </li>
         <li>
-          <button className="dropdown-item">Another action</button>
+          <button className="dropdown-item">دسته</button>
         </li>
         <li>
-          <button className="dropdown-item">Something else here</button>
+          <button className="dropdown-item">بندی</button>
         </li>
       </ul>
     </div>
