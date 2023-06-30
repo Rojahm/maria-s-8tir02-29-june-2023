706aba0180020e57a7034e7d8146e962e3b12e88
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-06-30 17:22:21 +0330

    added page module

--- src/App.js
+++ src/App.js
@@ -2,26 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/Footer";
 import Menu from "./components/Menu";
+import Page from "./components/Page";
 
 function App() {
   return (
     <div className="App">
       <Menu />
-      <h2>page</h2>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
-      <p>content</p>
+      <Page />
       <Footer />
     </div>
   );
---
+++ src/components/Page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Page.css";
+
+const Page = () => {
+  return (
+    <div className="Page">
+      <div className="container"> </div>
+    </div>
+  );
+};
+
+export default Page;
