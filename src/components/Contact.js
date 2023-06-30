7f992eb69ffbd25c2d6773297d51c80d9acec9b8
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-06-30 12:38:15 +0330

    contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,10 @@ const Contact = () => {
       <div className="row">
         <div className="col">
           <h4>تماس</h4>
+          <p>
+            تماس با روژا: 021 5782 6000 شنبه تا چهارشنبه از ساعت 8:30 تا 17:00 |
+            پنچشنبه ها از 8:30 تا 13
+          </p>
         </div>
         <div className="col">از تخفیف‌ها و جدیدترین‌ها باخبر شوید:</div>
         <div className="col"> در شبکه های اجتماعی</div>
