e9f68e5ddd4c24f2042bf15f450d815bafec42ce
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-06-30 17:23:13 +0330

    + and - works and no minus 0

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import React from "react";
 import { BiMinus, BiPlus } from "react-icons/bi";
 
 const CartItem = () => {
+  const [itemNum, SetItemNum] = useState(0);
+
   return (
     <div className="CartItem">
       <li className="Item">
@@ -14,13 +17,22 @@ const CartItem = () => {
         </div>
         <div>
           <p>استیک دئودورانت شیر و عسل دیپ سنس</p>
+          <p>۶۳,۹۲۰ تومان</p>
         </div>
         <div className="ItemNumber">
-          <div className="ItemNumberAdd">
+          <div
+            className="ItemNumberAdd"
+            onClick={() => SetItemNum(itemNum + 1)}
+          >
             <BiPlus />
           </div>
-          <div>1</div>
-          <div className="ItemNumberMinus">
+          <div>{itemNum}</div>
+          <div
+            className="ItemNumberMinus"
+            onClick={() =>
+              itemNum !== 0 ? SetItemNum(itemNum - 1) : SetItemNum(0)
+            }
+          >
             <BiMinus />
           </div>
         </div>
