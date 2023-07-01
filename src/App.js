460afebd8e65588e5e924cbedb176ce43aed44cb
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-07-01 20:48:14 +0330

    api stuff

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Menu from "./components/Menu";
 import Page from "./components/Page";
 
 function App() {
+  const apiKey =
+    "kUZkjj1YMW6CM4G5ZaeODZlc6ERMep7acQAGp3yNYSLEvUnbWfvIH3fHrHm4wGKp";
+  const endpoint =
+    "https://eu-west-1.aws.data.mongodb-api.com/app/data-qnydh/endpoint/data/v1";
   return (
     <div className="App">
       <Menu />
